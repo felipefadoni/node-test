2a31b99917d12aa1de212a0aeeca45eefac33710
Felipe Fadoni <felipefadonimt@gmail.com>
2023-06-12 12:55:23 -0300

    frist commit

---
+++ app/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+app.get("/", (req, res, next) => {
+  res.json(
+    `Seja bem vindo ao VPS rodando Node.JS pelo Docker na porta ${port}`
+  );
+});
+
+app.get("/cdfs", (req, res, next) => {
+  res.json(["Felipe Fadoni", "Cibele Fadoni"]);
+});
